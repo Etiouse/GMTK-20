342bf5e5842da8880a7212b8343a652cc58cbeaf
Alexander Wohlfahrt <akawohlfahrt@gmail.com>
2020-07-11 23:27:51 +0200

    Create basic maze and spawn point logic

--- Assets/Scripts/BallMazeManager.cs
+++ Assets/Scripts/BallMazeManager.cs
@@ -11,11 +11,14 @@ public class BallMazeManager : MonoBehaviour
 
     private bool isBallOutOfPath;
 
+    private bool hasApplicationFocus;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cursorLocked = true;
         isBallOutOfPath = false;
+        MazeHandler.HighlightNextSpawn();
     }
 
     void Update()
@@ -23,7 +26,7 @@ public class BallMazeManager : MonoBehaviour
         if (isBallOutOfPath)
         {
             isBallOutOfPath = false;
-            SetBallPosition(Vector3.zero);
+            SetBallPosition(MazeHandler.GetCurrentSpawn().transform.position);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -41,12 +44,13 @@ public class BallMazeManager : MonoBehaviour
             Vector3 offset = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * mouseSpeed;
 
             RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, offset, offset.sqrMagnitude);
-            //Debug.DrawRay(transform.position, offset, Color.red, 5);
+            Debug.DrawRay(transform.position, offset, Color.red, 5);
 
             foreach (RaycastHit2D hit in hits)
             {
                 if (hit.collider.tag == "Wall")
                 {
+                    Debug.Log(hit.collider.name);
                     isBallOutOfPath = true;
                     break;
                 }
@@ -59,13 +63,46 @@ public class BallMazeManager : MonoBehaviour
         }
     }
 
-    private void MoveBall(Vector3 offset)
+    void OnApplicationFocus(bool hasFocus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void MoveBall(Vector3 offset)
     {
         transform.position += offset;
     }
 
-    private void SetBallPosition(Vector3 newPosition)
+    public void SetBallPosition(Vector3 newPosition)
     {
         transform.position = newPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Spawn")
+        {
+            int newSpawnIndex = collision.GetComponent<SpawnStorage>().SpawnIndex;
+            Debug.Log(newSpawnIndex + " " + MazeHandler.CurrentSpawnIndex);
+
+            if (newSpawnIndex > MazeHandler.CurrentSpawnIndex)
+            {
+                MazeHandler.CurrentSpawnIndex = newSpawnIndex;
+
+                if (newSpawnIndex >= MazeHandler.GetMaxSpawnNumber() - 1)
+                {
+                    Debug.Log("Win");
+                }
+                else
+                {
+                    MazeHandler.HighlightNextSpawn();
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/SpawnStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStorage : MonoBehaviour
+{
+    [SerializeField]
+    public int SpawnIndex;
+}
