bb9e5a3892b073c6bfc88101e2f6ab1c47e20c57
Farid Abdalla <farid.abdalla.13@gmail.com>
2020-07-11 15:39:28 +0200

    Fake cursor for the labyrinth

---
+++ Assets/Scripts/MazeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    public CursorHandler cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cursor.MoveTo(Vector3.zero);
+        }
+    }
+}
