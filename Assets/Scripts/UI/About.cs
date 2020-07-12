7d8795f2e5ac9e1708757f31dbad81ba684b5dab
Etienne Hüsler <husleret@gmail.com>
2020-07-12 19:47:57 +0200

    empty about window

---
+++ Assets/Scripts/UI/About.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class About : MonoBehaviour
+{
+    [SerializeField] private GameObject aboutWindow = null;
+
+    public void Show()
+    {
+        aboutWindow.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        aboutWindow.SetActive(false);
+    }
+}
